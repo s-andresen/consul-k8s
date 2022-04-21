61e97b3db1d947e6046b6722c10adf47972e2619
Thomas Eckert <teckert@hashicorp.com>
2022-04-21 13:57:23 -0400

    Get Port forwarding kind of working

--- cli/cmd/proxy/list/list.go
+++ cli/cmd/proxy/list/list.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/hashicorp/consul-k8s/cli/common"
+	"github.com/hashicorp/consul-k8s/cli/common/flag"
 	"github.com/hashicorp/consul-k8s/cli/common/terminal"
 	helmCLI "helm.sh/helm/v3/pkg/cli"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,19 +17,43 @@ type Command struct {
 
 	kubernetes kubernetes.Interface
 
+	set *flag.Sets
+
+	flagKubeConfig  string
+	flagKubeContext string
+
 	once sync.Once
 	help string
 }
 
 func (c *Command) init() {
+	c.set = flag.NewSets()
+	f := c.set.NewSet("GlobalOptions")
+	f.StringVar(&flag.StringVar{
+		Name:    "kubeconfig",
+		Aliases: []string{"c"},
+		Target:  &c.flagKubeConfig,
+		Default: "",
+		Usage:   "Set the path to kubeconfig file.",
+	})
+	f.StringVar(&flag.StringVar{
+		Name:    "context",
+		Target:  &c.flagKubeContext,
+		Default: "",
+		Usage:   "Set the Kubernetes context to use.",
+	})
+
 	c.Init()
 }
 
 func (c *Command) Run(args []string) int {
+	namespace := "default"
+
 	c.once.Do(c.init)
 	c.Log.ResetNamed("list")
 	defer common.CloseWithError(c.BaseCommand)
 
+	// TODO I shouldn't use the Helm CLI here...
 	settings := helmCLI.New()
 
 	// Set up the kubernetes client to use for non Helm SDK calls to the Kubernetes API
@@ -48,10 +73,33 @@ func (c *Command) Run(args []string) int {
 	}
 
 	// Get all of the pods in the namespace
-	pods, err := c.kubernetes.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+	pods, err := c.kubernetes.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		c.UI.Output(err.Error())
 		return 1
+	}
+
+	// Open the port-forward to each of the pods
+	portForwardSessions := make([]*common.PortForward, len(pods.Items))
+	for _, pod := range pods.Items {
+		pf := common.PortForward{
+			Namespace:   namespace,
+			PodName:     pod.Name,
+			RemotePort:  19000,
+			UI:          c.UI,
+			KubeClient:  c.kubernetes,
+			KubeConfig:  "/Users/thomaseckert/.kube/config",
+			KubeContext: "kind-kind",
+		}
+
+		err := pf.Open()
+		defer pf.Close()
+		if err != nil {
+			c.UI.Output(err.Error())
+			return 1
+		}
+
+		portForwardSessions = append(portForwardSessions, &pf)
 	}
 
 	table := map[string][]string{
@@ -61,6 +109,7 @@ func (c *Command) Run(args []string) int {
 	}
 	for _, pod := range pods.Items {
 		table["Name"] = append(table["Name"], pod.Name)
+		// TODO actually read the proxy type
 		table["Type"] = append(table["Type"], "Service")
 		table["Status"] = append(table["Status"], "Healthy")
 	}
---
+++ cli/common/portforward.go
@@ -0,0 +1,94 @@
+package common
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+
+	"github.com/hashicorp/consul-k8s/cli/common/terminal"
+	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+)
+
+type PortForward struct {
+	Namespace  string
+	PodName    string
+	RemotePort int
+
+	UI terminal.UI
+
+	// TODO these three items tend to travel together, maybe abstract into a struct?
+	KubeClient  kubernetes.Interface
+	KubeConfig  string
+	KubeContext string
+
+	localPort int
+	stopChan  chan struct{}
+	readyChan chan struct{}
+}
+
+func (pf *PortForward) Open() error {
+	// TODO replace this with "freeport"
+	// Generate random port between 49152 and 65535 (dynamic ports)
+	pf.localPort = rand.Intn(65535-49152) + 49152
+
+	config, err := loadApiClientConfig(pf.KubeConfig, pf.KubeContext)
+
+	postEndpoint := pf.KubeClient.CoreV1().RESTClient().Post()
+	portForwardCreateURL := postEndpoint.
+		Resource("pods").
+		Namespace(pf.Namespace).
+		Name(pf.PodName).
+		SubResource("portforward").
+		URL()
+
+	transport, upgrader, err := spdy.RoundTripperFor(config)
+	if err != nil {
+		return err
+	}
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", portForwardCreateURL)
+
+	ports := []string{fmt.Sprintf("%d:%d", pf.localPort, pf.RemotePort)}
+	portforwarder, err := portforward.New(dialer, ports, pf.stopChan, pf.readyChan, os.Stdout, os.Stderr)
+	if err != nil {
+		return err
+	}
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- portforwarder.ForwardPorts()
+	}()
+
+	select {
+	case err := <-errChan:
+		return err
+	case <-pf.readyChan:
+		return nil
+	}
+}
+
+func (pf *PortForward) Endpoint() string {
+	// Need to check if open first
+	return fmt.Sprintf("localhost:%d", pf.localPort)
+}
+
+func (pf *PortForward) Close() {
+	close(pf.stopChan)
+}
+
+func loadApiClientConfig(kubeConfig, kubeContext string) (*restclient.Config, error) {
+	overrides := clientcmd.ConfigOverrides{}
+	if kubeContext != "" {
+		overrides.CurrentContext = kubeContext
+	}
+
+	config := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfig},
+		&overrides)
+
+	return config.ClientConfig()
+}
