0258fa7c02a9a41d8965620e0d8cd86d45eec7e2
Thomas Eckert <teckert@hashicorp.com>
2022-05-16 14:10:39 -0400

    Fix output for read cmd

--- cli/cmd/proxy/read/command.go
+++ cli/cmd/proxy/read/command.go
@@ -37,6 +37,7 @@ type Command struct {
 
 	flagPodName   string
 	flagNamespace string
+	flagJSON      bool
 
 	flagKubeConfig  string
 	flagKubeContext string
@@ -64,6 +65,12 @@ func (c *Command) init() {
 		Default: "default",
 		Usage:   "The namespace to list proxies in.",
 		Aliases: []string{"n"},
+	})
+	f.BoolVar(&flag.BoolVar{
+		Name:    "json",
+		Target:  &c.flagJSON,
+		Default: false,
+		Usage:   "Output the whole Envoy Config as JSON.",
 	})
 
 	f = c.set.NewSet("GlobalOptions")
@@ -155,10 +162,14 @@ func (c *Command) Run(args []string) int {
 	}
 
 	c.UI.Output(fmt.Sprintf("%s Proxy Configuration", c.flagPodName), terminal.WithHeaderStyle())
-	err = c.Print(config)
-	if err != nil {
-		c.UI.Output("Error printing config:\n%v", err, terminal.WithErrorStyle())
-		return 1
+	if c.flagJSON {
+		c.UI.Output(string(config))
+	} else {
+		err = c.Print(config)
+		if err != nil {
+			c.UI.Output("Error printing config:\n%v", err, terminal.WithErrorStyle())
+			return 1
+		}
 	}
 
 	return 0
@@ -208,69 +219,22 @@ func (c *Command) PrintClusters(clusters map[string]interface{}) error {
 
 	tbl := terminal.NewTable("Name", "FQDN", "Endpoints", "Type", "Last Updated")
 
-	for _, cluster := range clusters["static_clusters"].([]interface{}) {
-		a := cluster.(map[string]interface{})
-		b := a["cluster"].(map[string]interface{})
-		fqdn := b["name"].(string)
-		var name string
-		if strings.Contains(fqdn, ".") {
-			name = strings.Split(fqdn, ".")[0]
-		} else {
-			name = fqdn
-		}
-		load_assignment := b["load_assignment"].(map[string]interface{})
-		eps := load_assignment["endpoints"].([]interface{})
+	if clusters["static_clusters"] != nil {
 
-		endpoints := make([]string, 0)
-		for _, ep := range eps {
-			lb_endpoints := ep.(map[string]interface{})["lb_endpoints"].([]interface{})
-			for _, lb_ep := range lb_endpoints {
-				e := lb_ep.(map[string]interface{})
-				f := e["endpoint"].(map[string]interface{})
-				address := f["address"].(map[string]interface{})
-				sockaddr := address["socket_address"].(map[string]interface{})
-				addr := sockaddr["address"].(string)
-				portv := int(sockaddr["port_value"].(float64))
-				endpoints = append(endpoints, fmt.Sprintf("%s:%d", addr, portv))
+		for _, cluster := range clusters["static_clusters"].([]interface{}) {
+			a := cluster.(map[string]interface{})
+			b := a["cluster"].(map[string]interface{})
+			fqdn := b["name"].(string)
+
+			var name string
+			if strings.Contains(fqdn, ".") {
+				name = strings.Split(fqdn, ".")[0]
+			} else {
+				name = fqdn
 			}
-		}
-
-		typ := b["type"].(string)
-		lupdated := a["last_updated"].(string)
-		trow := []terminal.TableEntry{
-			{
-				Value: name,
-			},
-			{
-				Value: fqdn,
-			},
-			{
-				Value: strings.Join(endpoints, ", "),
-			},
-			{
-				Value: typ,
-			},
-			{
-				Value: lupdated,
-			},
-		}
-		tbl.Rows = append(tbl.Rows, trow)
-	}
-	for _, cluster := range clusters["dynamic_active_clusters"].([]interface{}) {
-		a := cluster.(map[string]interface{})
-		b := a["cluster"].(map[string]interface{})
-		fqdn := b["name"].(string)
-		var name string
-		if strings.Contains(fqdn, ".") {
-			name = strings.Split(fqdn, ".")[0]
-		} else {
-			name = fqdn
-		}
-
-		endpts := ""
-		if b["load_assignment"] != nil {
 			load_assignment := b["load_assignment"].(map[string]interface{})
 			eps := load_assignment["endpoints"].([]interface{})
+
 			endpoints := make([]string, 0)
 			for _, ep := range eps {
 				lb_endpoints := ep.(map[string]interface{})["lb_endpoints"].([]interface{})
@@ -284,29 +248,82 @@ func (c *Command) PrintClusters(clusters map[string]interface{}) error {
 					endpoints = append(endpoints, fmt.Sprintf("%s:%d", addr, portv))
 				}
 			}
-			endpts = strings.Join(endpoints, ", ")
-		}
 
-		typ := b["type"].(string)
-		lupdated := a["last_updated"].(string)
-		trow := []terminal.TableEntry{
-			{
-				Value: name,
-			},
-			{
-				Value: fqdn,
-			},
-			{
-				Value: endpts,
-			},
-			{
-				Value: typ,
-			},
-			{
-				Value: lupdated,
-			},
+			typ := b["type"].(string)
+			lupdated := a["last_updated"].(string)
+			trow := []terminal.TableEntry{
+				{
+					Value: name,
+				},
+				{
+					Value: fqdn,
+				},
+				{
+					Value: strings.Join(endpoints, ", "),
+				},
+				{
+					Value: typ,
+				},
+				{
+					Value: lupdated,
+				},
+			}
+			tbl.Rows = append(tbl.Rows, trow)
 		}
-		tbl.Rows = append(tbl.Rows, trow)
+	}
+	if clusters["dynamic_active_clusters"] != nil {
+		for _, cluster := range clusters["dynamic_active_clusters"].([]interface{}) {
+			a := cluster.(map[string]interface{})
+			b := a["cluster"].(map[string]interface{})
+			fqdn := b["name"].(string)
+			var name string
+			if strings.Contains(fqdn, ".") {
+				name = strings.Split(fqdn, ".")[0]
+			} else {
+				name = fqdn
+			}
+
+			endpts := ""
+			if b["load_assignment"] != nil {
+				load_assignment := b["load_assignment"].(map[string]interface{})
+				eps := load_assignment["endpoints"].([]interface{})
+				endpoints := make([]string, 0)
+				for _, ep := range eps {
+					lb_endpoints := ep.(map[string]interface{})["lb_endpoints"].([]interface{})
+					for _, lb_ep := range lb_endpoints {
+						e := lb_ep.(map[string]interface{})
+						f := e["endpoint"].(map[string]interface{})
+						address := f["address"].(map[string]interface{})
+						sockaddr := address["socket_address"].(map[string]interface{})
+						addr := sockaddr["address"].(string)
+						portv := int(sockaddr["port_value"].(float64))
+						endpoints = append(endpoints, fmt.Sprintf("%s:%d", addr, portv))
+					}
+				}
+				endpts = strings.Join(endpoints, ", ")
+			}
+
+			typ := b["type"].(string)
+			lupdated := a["last_updated"].(string)
+			trow := []terminal.TableEntry{
+				{
+					Value: name,
+				},
+				{
+					Value: fqdn,
+				},
+				{
+					Value: endpts,
+				},
+				{
+					Value: typ,
+				},
+				{
+					Value: lupdated,
+				},
+			}
+			tbl.Rows = append(tbl.Rows, trow)
+		}
 	}
 	c.UI.Table(tbl)
 
@@ -318,41 +335,64 @@ func (c *Command) PrintListeners(listeners map[string]interface{}) error {
 
 	tbl := terminal.NewTable("Name", "Address:Port", "Direction", "Filter Chain Match", "Destination Cluster", "Last Updated")
 
-	for _, listener := range listeners["dynamic_listeners"].([]interface{}) {
-		l := listener.(map[string]interface{})
-		al := l["active_state"].(map[string]interface{})
-		name := strings.Split(l["name"].(string), ":")[0]
-		addr := strings.SplitN(l["name"].(string), ":", 2)[1]
-		l2 := al["listener"].(map[string]interface{})
-		direction := l2["traffic_direction"].(string)
-		fcm := []string{}
-		dest := []string{}
-		lastUpdated := al["last_updated"].(string)
+	if listeners["dynamic_listeners"] != nil {
 
-		if direction == "OUTBOUND" {
-			fcs := l2["filter_chains"].([]interface{})
-			for _, fc := range fcs {
-				fc_ := fc.(map[string]interface{})
-				if fc_["filter_chain_match"] != nil {
-					fcmtch := fc_["filter_chain_match"].(map[string]interface{})
-					prs := fcmtch["prefix_ranges"].([]interface{})
-					for _, pr := range prs {
-						pr_ := pr.(map[string]interface{})
-						fcm = append(fcm, pr_["address_prefix"].(string))
+		for _, listener := range listeners["dynamic_listeners"].([]interface{}) {
+			l := listener.(map[string]interface{})
+			al := l["active_state"].(map[string]interface{})
+			name := strings.Split(l["name"].(string), ":")[0]
+			addr := strings.SplitN(l["name"].(string), ":", 2)[1]
+			l2 := al["listener"].(map[string]interface{})
+			direction := l2["traffic_direction"].(string)
+			fcm := []string{}
+			dest := []string{}
+			lastUpdated := al["last_updated"].(string)
+
+			if direction == "OUTBOUND" {
+				fcs := l2["filter_chains"].([]interface{})
+				for _, fc := range fcs {
+					fc_ := fc.(map[string]interface{})
+					if fc_["filter_chain_match"] != nil {
+						fcmtch := fc_["filter_chain_match"].(map[string]interface{})
+						prs := fcmtch["prefix_ranges"].([]interface{})
+						for _, pr := range prs {
+							pr_ := pr.(map[string]interface{})
+							fcm = append(fcm, pr_["address_prefix"].(string))
+						}
 					}
-				}
-				if fc_["filters"] != nil {
-					fltrs := fc_["filters"].([]interface{})
-					for _, fltr := range fltrs {
-						fltr_ := fltr.(map[string]interface{})
-						tc := fltr_["typed_config"].(map[string]interface{})
-						dest = append(dest, tc["cluster"].(string))
+					if fc_["filters"] != nil {
+						fltrs := fc_["filters"].([]interface{})
+						for _, fltr := range fltrs {
+							fltr_ := fltr.(map[string]interface{})
+							if fltr_["typed_config"] != nil {
+								tc := fltr_["typed_config"].(map[string]interface{})
+								if tc["cluster"] != nil {
+									dest = append(dest, tc["cluster"].(string))
+								}
+								if tc["route_config"] != nil {
+									rc := tc["route_config"].(map[string]interface{})
+									if rc["virtual_hosts"] != nil {
+										vhs := rc["virtual_hosts"].([]interface{})
+										for _, vh := range vhs {
+											vh_ := vh.(map[string]interface{})
+											if vh_["routes"] != nil {
+												rts := vh_["routes"].([]interface{})
+												for _, rt := range rts {
+													rt_ := rt.(map[string]interface{})
+													r := rt_["route"].(map[string]interface{})
+													dest = append(dest, r["cluster"].(string))
+												}
+											}
+										}
+									}
+								}
+							}
+						}
 					}
 				}
 			}
+			tbl.Rich([]string{name, addr, direction, strings.Join(fcm, ", "), strings.Join(dest, ", "), lastUpdated}, []string{})
 		}
-
-		tbl.Rich([]string{name, addr, direction, strings.Join(fcm, ", "), strings.Join(dest, ", "), lastUpdated}, []string{})
 	}
 
 	c.UI.Table(tbl)
